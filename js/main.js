9045f7649aa7200007c995192c205e1d86412792
Soumik Ahammed <46122468+soumik9@users.noreply.github.com>
2022-03-01 03:41:26 +0600

    show data in ui and if no data available on search value then show error

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ const searchPhone = () =>{
 
     if(searchField.value == ''){
         document.getElementById('error-msg').style.display = 'block';
-        document.getElementById('error-msg').innerText = 'Kichu toh likho naile khelbona';
+        document.getElementById('result-msg').style.display = 'none';
+        document.getElementById('error-msg').innerText = 'Kichu toh likho naile khelbona 😿';
     }else{
         document.getElementById('error-msg').style.display = 'none';
 
@@ -17,6 +18,53 @@ const searchPhone = () =>{
     
         fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displayPhones(data.data));
+    }
+}
+
+// display phone data on website
+const displayPhones = (phones) => {
+
+    const phonesContainer = document.getElementById('phones-container');
+
+    // showing in ui how many result we get
+    const lengthArr = phones.length
+    document.getElementById('result-msg').innerHTML = `<p class="text-primary">Total result: ${lengthArr} 🤖</p>`;
+
+    console.log(phones);
+
+    if(lengthArr < 1){
+        // check is there any data based on search if no then show error
+        document.getElementById('error-msg').style.display = 'block';
+        document.getElementById('result-msg').style.display = 'block';
+        document.getElementById('error-msg').innerText = 'Tumi ja likhso tar sathe miliye kicchu pawa jaini 😿';
+    }else{
+        document.getElementById('error-msg').style.display = 'none';
+        document.getElementById('result-msg').style.display = 'block';
+
+        // showing data
+        phones.forEach(phone => {
+            // create a div and adding class
+            const div = document.createElement('div');
+            div.classList = 'col-xl-4 col-md-4 col-sm-12';
+    
+            // showing data 
+            div.innerHTML = 
+                `<div class="card mb-3 p-3" style="max-width: 540px;">
+                        <div class="row justify-content-between g-0">
+                            <div class="col-md-5 h-100">
+                                <img src="${phone.image}" class="img-fluid rounded-start" alt="...">
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card-body">
+                                    <h1 class="card-title">${phone.brand}</h1>
+                                    <h5 class="card-title">${phone.phone_name}</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+            // appending data
+            phonesContainer.appendChild(div);
+        });
     }
 }
