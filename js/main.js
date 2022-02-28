42be4e6ba3e3baf00f2b6a05382f9c9115d9411e
Soumik Ahammed <46122468+soumik9@users.noreply.github.com>
2022-03-01 03:53:46 +0600

    clear previous result when hiting next search

--- js/main.js
+++ js/main.js
@@ -3,9 +3,13 @@ const searchPhone = () =>{
     const searchField = document.getElementById('search-field');
 
     if(searchField.value == ''){
-        document.getElementById('error-msg').style.display = 'block';
         document.getElementById('result-msg').style.display = 'none';
+        document.getElementById('error-msg').style.display = 'block';
         document.getElementById('error-msg').innerText = 'Kichu toh likho naile khelbona 😿';
+
+        // clear view data when showing error
+        const phonesContainer = document.getElementById('phones-container');
+        phonesContainer.textContent = '';
     }else{
         document.getElementById('error-msg').style.display = 'none';
 
@@ -26,6 +30,7 @@ const searchPhone = () =>{
 const displayPhones = (phones) => {
 
     const phonesContainer = document.getElementById('phones-container');
+    phonesContainer.textContent = '';
 
     // showing in ui how many result we get
     const lengthArr = phones.length
@@ -41,23 +46,24 @@ const displayPhones = (phones) => {
     }else{
         document.getElementById('error-msg').style.display = 'none';
         document.getElementById('result-msg').style.display = 'block';
-
+        
         // showing data
         phones.forEach(phone => {
             // create a div and adding class
             const div = document.createElement('div');
-            div.classList = 'col-xl-4 col-md-4 col-sm-12';
-    
+            div.classList.add('col-xl-4', 'col-md-4', 'col-sm-12')
+       
+
             // showing data 
             div.innerHTML = 
                 `<div class="card mb-3 p-3" style="max-width: 540px;">
                         <div class="row justify-content-between g-0">
-                            <div class="col-md-5 h-100">
+                            <div class="col-md-5 col-5 h-100">
                                 <img src="${phone.image}" class="img-fluid rounded-start" alt="...">
                             </div>
-                            <div class="col-md-6">
+                            <div class="col-md-6 col-6">
                                 <div class="card-body">
-                                    <h1 class="card-title">${phone.brand}</h1>
+                                    <h2 class="card-title">${phone.brand}</h2>
                                     <h5 class="card-title">${phone.phone_name}</h5>
                                 </div>
                             </div>
