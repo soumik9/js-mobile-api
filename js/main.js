4e2b266ffea56c5d83f36a5ba45e0e8948321af4
Soumik Ahammed <46122468+soumik9@users.noreply.github.com>
2022-03-02 03:19:00 +0600

    responsive for mobile fix

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const searchPhone = () =>{
         document.getElementById('spinner').style.display = 'none';
         document.getElementById('result-msg').style.display = 'none';
         document.getElementById('error-msg').style.display = 'block';
-        document.getElementById('error-msg').innerText = 'Kichu toh likho naile khelbona 😿';
+        document.getElementById('error-msg').innerText = 'Give some input to get result!';
 
         // clear view data when showing error
         document.getElementById('phones-container').textContent = '';
@@ -60,6 +60,7 @@ function displayResult(phone, phonesContainer){
     // appending data
     phonesContainer.appendChild(div);
 }
+
 const displayPhones = (phones) => {
     // spinner display change
     document.getElementById('spinner').style.display = 'none';
@@ -71,12 +72,14 @@ const displayPhones = (phones) => {
 
     if(phones.length < 1){
         // check is there any data based on search if no then show error
-        document.getElementById('result-msg').innerHTML = `<p class="text-primary">Total result: ${phones.length} 🤖</p>`;
+        document.getElementById('result-msg').style.display = 'block';
+        document.getElementById('result-msg').innerHTML = `<p class="text-primary">Total result: ${phones.length}</p>`;
         document.getElementById('error-msg').style.display = 'block';
-        document.getElementById('error-msg').innerText = 'Tumi ja likhso tar sathe miliye kicchu pawa jaini 😿';
+        document.getElementById('error-msg').innerText = 'No data found based on your search';
     }else{
         // showing only 20 result with load more option
         const limitPhones = phones.slice(0, 20);
+        document.getElementById('result-msg').style.display = 'block';
         document.getElementById('result-msg').innerHTML = `<p class="text-primary">Total result: ${phones.length}, Showing result: ${limitPhones.length ? limitPhones.length : phones.length} 🤖</p>`;
 
         limitPhones.forEach(phone => {
@@ -106,42 +109,41 @@ const loadPhoneDetails = (slug) => {
 const displayPhoneDetails = (phone) => {
     const phoneDetailsContainer = document.getElementById('phone-details-container');
     phoneDetailsContainer.textContent = '';
-
     // create a div and adding class
     const div = document.createElement('div');
-    div.classList.add('col-xl-12', 'col-md-12', 'col-sm-12');
+    div.classList.add('col-xl-12', 'col-md-12', 'col-sm-12', 'col-12', 'mb-2');
 
     // showing data 
     div.innerHTML = 
         `<div class="card mb-3 p-3">
-                <div class="row">
-                <div class="col-md-4 col-5">
+                <div class="row justify-content-lg-start justify-content-center">
+                <div class="col-md-4 col-4">
                     <img src="${phone.image}" class="img-fluid rounded-start h-75 alt="...">
                 </div>
-                <div class="col-md-8 col-8">
+                <div class="col-md-8 col-12">
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-md-5">
+                            <div class="col-md-5 col-6">
                                 <h4 class="card-title">Brand name</h4>
                             </div>
-                            <div class="col-md-7">
+                            <div class="col-md-7 col-6">
                                 <h4 class="card-title">${phone.brand}</h4>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-md-5">
+                            <div class="col-md-5 col-6">
                                 <h5 class="card-title">Phone name</h5>
                             </div>
-                            <div class="col-md-7">
+                            <div class="col-md-7 col-6">
                                 <h5 class="card-title">${phone.name}</h5>
                             </div>
                         </div>
                     
                         <div class="row">
-                            <div class="col-md-5">
+                            <div class="col-md-5 col-6">
                                 <p class="card-title">Relase Date</p>
                             </div>
-                            <div class="col-md-7">
+                            <div class="col-md-7 col-6">
                                  <p class="card-title">${phone.releaseDate ? phone.releaseDate : 'No release date is found'}</p>
                             </div>
                         </div>
@@ -151,42 +153,42 @@ const displayPhoneDetails = (phone) => {
                         </div>
                         <hr class="mb-1">
                         <div class="row">
-                            <div class="col-md-5">
+                            <div class="col-md-5 col-6">
                                 <p class="card-title">Storage</p>
                             </div>
-                            <div class="col-md-7">
+                            <div class="col-md-7 col-6">
                                  <p class="card-title">${phone.mainFeatures.storage ? phone.mainFeatures.storage : 'No storage data found'}</p>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-md-5">
+                            <div class="col-md-5 col-6">
                                 <p class="card-title">Display</p>
                             </div>
-                            <div class="col-md-7">
+                            <div class="col-md-7 col-6">
                                  <p class="card-title">${phone.mainFeatures.displaySize ? phone.mainFeatures.displaySize : 'No display data found'}</p>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-md-5">
+                            <div class="col-md-5 col-6">
                                 <p class="card-title">Chipset</p>
                             </div>
-                            <div class="col-md-7">
+                            <div class="col-md-7 col-6">
                                  <p class="card-title">${phone.mainFeatures.chipSet ? phone.mainFeatures.chipSet : 'No chipSet data found'}</p>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-md-5">
+                            <div class="col-md-5 col-6">
                                 <p class="card-title">Memory</p>
                             </div>
-                            <div class="col-md-7">
+                            <div class="col-md-7 col-6">
                                  <p class="card-title">${phone.mainFeatures.memory ? phone.mainFeatures.memory : 'No memory data found'}</p>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-md-5">
+                            <div class="col-md-5 col-6">
                                 <p class="card-title">Sensors</p>
                             </div>
-                            <div class="col-md-7">
+                            <div class="col-md-7 col-6">
                                  <p class="card-title">${phone.mainFeatures.sensors.map(el => el).join(', ')}</p>
                             </div>
                         </div>
@@ -195,54 +197,57 @@ const displayPhoneDetails = (phone) => {
                             <h4>Others</h4>
                         </div>
                         <hr class="mb-1">
-                        <div class="row">
-                            <div class="col-md-5">
-                                <p class="card-title">Bluetooth</p>
+
+                        ${phone.others ? 
+                            `<div class="row">
+                                <div class="col-md-5 col-6">
+                                    <p class="card-title">Bluetooth</p>
+                                </div>
+                                <div class="col-md-7 col-6">
+                                    <p class="card-title">${phone.others.Bluetooth ? phone.others.Bluetooth : 'No bluetooth data found'}</p>
+                                </div>
                             </div>
-                            <div class="col-md-7">
-                                 <p class="card-title">${phone.others.Bluetooth ? phone.others.Bluetooth : 'No bluetooth data found'}</p>
+                            <div class="row">
+                                <div class="col-md-5 col-6">
+                                    <p class="card-title">GPS</p>
+                                </div>
+                                <div class="col-md-7 col-6">
+                                    <p class="card-title">${phone.others.GPS ? phone.others.GPS : 'No GPS data found'}</p>
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-5">
-                                <p class="card-title">GPS</p>
+                            <div class="row">
+                                <div class="col-md-5 col-6">
+                                    <p class="card-title">NFC</p>
+                                </div>
+                                <div class="col-md-7 col-6">
+                                    <p class="card-title">${phone.others.NFC ? phone.others.NFC : 'No NFC data found'}</p>
+                                </div>
                             </div>
-                            <div class="col-md-7">
-                                 <p class="card-title">${phone.others.GPS ? phone.others.GPS : 'No GPS data found'}</p>
+                            <div class="row">
+                                <div class="col-md-5 col-6">
+                                    <p class="card-title">Radio</p>
+                                </div>
+                                <div class="col-md-7 col-6">
+                                    <p class="card-title">${phone.others.Radio ? phone.others.Radio : 'No Radio data found'}</p>
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-5">
-                                <p class="card-title">NFC</p>
+                            <div class="row">
+                                <div class="col-md-5 col-6">
+                                    <p class="card-title">USB</p>
+                                </div>
+                                <div class="col-md-7 col-6">
+                                    <p class="card-title">${phone.others.USB ? phone.others.USB : 'No USB data found'}</p>
+                                </div>
                             </div>
-                            <div class="col-md-7">
-                                 <p class="card-title">${phone.others.NFC ? phone.others.NFC : 'No NFC data found'}</p>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-5">
-                                <p class="card-title">Radio</p>
-                            </div>
-                            <div class="col-md-7">
-                                 <p class="card-title">${phone.others.Radio ? phone.others.Radio : 'No Radio data found'}</p>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-5">
-                                <p class="card-title">USB</p>
-                            </div>
-                            <div class="col-md-7">
-                                 <p class="card-title">${phone.others.USB ? phone.others.USB : 'No USB data found'}</p>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-5">
-                                <p class="card-title">WLAN</p>
-                            </div>
-                            <div class="col-md-7">
-                                 <p class="card-title">${phone.others.WLAN ? phone.others.WLAN : 'No WLAN data found'}</p>
-                            </div>
-                        </div>
+                            <div class="row">
+                                <div class="col-md-5 col-6">
+                                    <p class="card-title">WLAN</p>
+                                </div>
+                                <div class="col-md-7 col-6">
+                                    <p class="card-title">${phone.others.WLAN ? phone.others.WLAN : 'No WLAN data found'}</p>
+                                </div>
+                            </div>` : 'No others data found'}
+ 
                     </div>
                 </div>
             </div>
@@ -272,7 +277,7 @@ const displayAllPhones = (phones) => {
     loadContainer.textContent = '';
 
     document.getElementById('error-msg').style.display = 'none';
-    document.getElementById('result-msg').innerHTML = `<p class="text-primary">Total result: ${phones.length}, Showing result: ${phones.length} 🤖</p>`;
+    document.getElementById('result-msg').innerHTML = `<p class="text-primary">Total result: ${phones.length}, Showing result: ${phones.length}</p>`;
 
     // showing data
     phones.forEach(phone => {
